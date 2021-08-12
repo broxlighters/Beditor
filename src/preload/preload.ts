a960c095c30c3c2a0afae40f3c2503a2f149dfb9
broxlighters <broxlighters@gmail.com>
2021-08-12 22:19:55 +0800

    查看文本

---
+++ src/preload/preload.ts
@@ -0,0 +1,11 @@
+// import {ipcRenderer} from  'electron';
+// import os from "os";
+//
+// console.log(os.type());
+// ipcRenderer.send('os', os.type());
+
+// import { contextBridge, ipcRenderer } from 'electron';
+//
+// contextBridge.exposeInMainWorld('message',{
+//     loadPreferences: () => ipcRenderer.send('message', 'ping ping ping from render process')
+// })
